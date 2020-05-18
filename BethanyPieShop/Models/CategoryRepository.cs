0022735f360ed680098e7ab1e1e54fc8a75d7b9b
Darklline <ivaxy12@gmail.com>
2020-05-18 20:04:07 +0200

    Added EFC + dummy data Using DbContext

---
+++ BethanyPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Category> AllCategories => context.Categories;
+    }
+}
---
+++ BethanyPieShop/Models/PieRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext context;
+
+        public PieRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Pie> AllPies => context.Pies.Include(c => c.Category);
+
+        public IEnumerable<Pie> PiesOfTheWeek => context.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek);
+
+        public Pie GetPieById(int pieId)
+        {
+            return context.Pies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}

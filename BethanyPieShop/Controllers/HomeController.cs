9bfee0efe21c64107c8c5496b52b9c371cc8ccab
Darklline <ivaxy12@gmail.com>
2020-05-19 18:13:46 +0200

    Home Controller Index Action

---
+++ BethanyPieShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BethanyPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            this.pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = pieRepository.PiesOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
